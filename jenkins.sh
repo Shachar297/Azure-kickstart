fbaf4142c618d1e275d81678833d9a524e7dd560
Shachar297 <thisishachar@gmail.com>
2021-11-22 19:11:55 +0200

    commit

---
+++ jenkins.sh
@@ -0,0 +1,7 @@
+
+
+
+curl \
+	    -X POST \
+	        -L --user "admin:118b07f578983aac45e530fa7c212de965" \
+		    "http://localhost:8888/job/Azure-VM/buildWithParameters?RESOURCE_GROUP_NAME=ShacharWeb&VM_NAME=nodeJsApp&USER_NAME=Shachar&FIRST_IMAGE=UbuntuLTS"
