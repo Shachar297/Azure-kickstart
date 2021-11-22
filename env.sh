6472093f13d7b6d5f484706a21b6907f125b5faa
Shachar297 <thisishachar@gmail.com>
2021-11-22 19:27:11 +0200

    commit

--- env.sh
+++
@@ -1,3 +0,0 @@
-export RESOURCE_GROUP_NAME="ShacharFirst"
-export VM_NAME="FirstVM"
-export USER_NAME="Shachar"
--- resourceGroupCmd.sh
+++
@@ -1,7 +0,0 @@
-
-# Create Resource Group
-az group create --name groupName --location eastus
-
-# Delete Resource Group
-
-az group delete --name groupName
